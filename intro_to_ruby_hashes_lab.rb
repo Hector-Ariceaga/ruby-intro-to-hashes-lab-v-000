3142a68915d0cc76adc6a328052b3fdae2658e27
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-27 17:13:13 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ def monopoly_with_second_tier
 		    :pieces => 4,
 		    :names => {},
 		    :rent_in_dollars => {
-		      :one_piece_owned => {
+		      :one_piece_owned => {}
 		    }
-		}}
+		  }
 
 end
 
